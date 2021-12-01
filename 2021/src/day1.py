90726735836b5441dd1d44a9925d1464272168b8
Michael Grossfeld <pantaryl@gmail.com>
2021-12-01 00:15:58 -0500

    Day 1 Solution

--- 2021/src/day1.py
+++ 2021/src/day1.py
@@ -1,7 +1,24 @@
 from shared import *
 
 # Input data is in INPUT_DATA.
+INPUT_DATA = [int(x) for x in INPUT_DATA]
 
 # Part 1
+count = 0
+for i, val in enumerate(INPUT_DATA):
+    if i == 0: continue
+    if val > INPUT_DATA[i - 1]: count += 1
+
+print(count)
 
 # Part 2
+count = 0
+prevWindow = None
+curWindow  = 0
+for i, val in enumerate(INPUT_DATA):
+    if i < 2: continue
+    curWindow = INPUT_DATA[i - 2] + INPUT_DATA[i - 1] + INPUT_DATA[i]
+    if prevWindow and curWindow > prevWindow: count += 1
+    prevWindow = curWindow
+
+print(count)
