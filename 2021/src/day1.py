aec64d86921d0ffb0c8e5f03b3e429cd4dd17728
Michael Grossfeld <pantaryl@gmail.com>
2021-11-30 21:53:06 -0500

    Add initial setup for Day1.

---
+++ 2021/src/day1.py
@@ -0,0 +1,7 @@
+from shared import *
+
+# Input data is in INPUT_DATA.
+
+# Part 1
+
+# Part 2
--- shared/template.py
+++ shared/template.py
@@ -3,8 +3,14 @@ from helpers import memoize
 from copy import deepcopy
 from enum import Enum
 import os
-from math_utils import *
-from helpers import *
+from .math_utils import *
+from .helpers    import *
+
+import __main__
+
+_MAIN_FILE        =__main__.__file__
+_MAIN_DIR         = os.path.dirname(_MAIN_FILE)
+_MAIN_FILE_NO_EXT = os.path.splitext(os.path.basename(_MAIN_FILE))[0]
 
 # Complex numbers in Python! == i + nj (x + ny), good for coordinates
 # complex*+imaginary to rotate left/ccw, complex*-imaginary to rotate right/cw
@@ -12,6 +18,6 @@ from helpers import *
 # Rotate CCW by 270 degrees:
 #    val *= complex(0, 1) ** 3 [each power represents 90 degrees]
 
-with open(f"../input/{os.path.splitext(os.path.basename(__file__))[0]}.txt", 'r') as inputFile:
-    data = [x.rstrip() for x in inputFile.readlines()]
-    #data = [int(x) for x in data]
+INPUT_DATA = []
+with open(f"{_MAIN_DIR}/../input/{_MAIN_FILE_NO_EXT}.txt", 'r') as inputFile:
+    INPUT_DATA = [x.rstrip() for x in inputFile.readlines()]
