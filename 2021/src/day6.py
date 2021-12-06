e38086f78944867c51e2e05a336c82a57d2ab1c0
Michael Grossfeld <pantaryl@gmail.com>
2021-12-06 01:10:45 -0500

    Clean up Day6

--- 2021/src/day6.py
+++ 2021/src/day6.py
@@ -9,20 +9,17 @@ for i in range(0, 9):
     counts[i] = INPUT_DATA.count(i)
 
 for i in range(0, 256):
-    counts['temp8'] = counts[8]
-    counts['temp6'] = counts[6]
-    counts[8]       = counts[0]
-    counts[6]       = counts[0] + counts[7]
+    existing_zero   = counts[0]
 
     counts[0]       = counts[1]
     counts[1]       = counts[2]
     counts[2]       = counts[3]
     counts[3]       = counts[4]
     counts[4]       = counts[5]
-    counts[5]       = counts['temp6']
-    counts[7]       = counts['temp8']
+    counts[5]       = counts[6]
+    counts[6]       = existing_zero + counts[7]
+    counts[7]       = counts[8]
+    counts[8]       = existing_zero
 
     if i == 79 or i == 255:
-        del counts['temp6']
-        del counts['temp8']
         print(sum([x for _, x in counts.items()]))
