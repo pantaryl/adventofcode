cd515fa3445c033c8d303aea03d78120a4c4e855
Michael Grossfeld <pantaryl@gmail.com>
2021-12-06 01:26:16 -0500

    Upload extremely elegant solution for Day6

--- 2021/src/day6.py
+++ 2021/src/day6.py
@@ -3,23 +3,15 @@ from shared import *
 # Input data is in INPUT_DATA.
 INPUT_DATA = [int(x) for x in INPUT_DATA[0].split(",")]
 
-# Part 1
-counts = defaultdict(int)
+fishes = deque()
 for i in range(0, 9):
-    counts[i] = INPUT_DATA.count(i)
+    fishes.append(0)
+for fish in INPUT_DATA:
+    fishes[fish] += 1
 
 for i in range(0, 256):
-    existing_zero   = counts[0]
-
-    counts[0]       = counts[1]
-    counts[1]       = counts[2]
-    counts[2]       = counts[3]
-    counts[3]       = counts[4]
-    counts[4]       = counts[5]
-    counts[5]       = counts[6]
-    counts[6]       = existing_zero + counts[7]
-    counts[7]       = counts[8]
-    counts[8]       = existing_zero
+    fishes.rotate(-1)
+    fishes[6] += fishes[-1]
 
     if i == 79 or i == 255:
-        print(sum([x for _, x in counts.items()]))
+        print(sum(fishes))
