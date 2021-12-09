4a3e57735beca5bb82fbfe9ffaed5a9eb864c834
Michael Grossfeld <pantaryl@gmail.com>
2021-12-09 00:45:17 -0500

    Day9 Solution

---
+++ 2021/src/day9.py
@@ -0,0 +1,64 @@
+from shared import *
+
+# Input data is in INPUT_DATA.
+# INPUT_DATA = [int(x) for x in INPUT_DATA]
+caves = squareGridFromChars(INPUT_DATA, isInts=True)
+
+# Part 1
+
+lowPoints = []
+for pos, val in caves.items():
+    lower = True
+    for neighbor in getOrthagonalSquareNeighbors():
+        if pos + neighbor in caves:
+            if caves[pos + neighbor] <= val:
+                lower = False
+                break
+        if lower is False: break
+
+    if lower:
+        lowPoints.append((pos, val))
+
+print(sum([x[1] + 1 for x in lowPoints]))
+
+# Part 2
+lowPointPos = [x[0] for x in lowPoints]
+basins = []
+for pos in lowPointPos:
+    seen  = set()
+    stack = [ pos ]
+    while stack:
+        currentPos = stack.pop()
+        currentVal = caves[currentPos]
+        assert(currentVal != 9)
+        seen.add(currentPos)
+
+        for neighbor in getOrthagonalSquareNeighbors():
+            neighborPos = currentPos + neighbor
+            neighborVal = caves.get(neighborPos, None)
+            if neighborVal and neighborPos not in seen and neighborVal > currentVal and neighborVal != 9:
+                stack.append(neighborPos)
+
+    _debug = False
+    if _debug:
+        maxI = int(max([x.real for x in seen]))
+        minI = int(min([x.real for x in seen]))
+        maxJ = int(max([x.imag for x in seen]))
+        minJ = int(min([x.imag for x in seen]))
+
+        print(pos - complex(minI, minJ), pos)
+        for j in range(minJ, maxJ + 1):
+            for i in range(minI, maxI + 1):
+                if complex(i, j) not in seen:
+                    print('X', end='')
+                else:
+                    print(caves[complex(i, j)], end='')
+            print()
+        print()
+        print()
+
+
+    basins.append(len(seen))
+
+largest = sorted(basins, reverse=True)
+print(largest[0] * largest[1] * largest[2])
--- shared/helpers.py
+++ shared/helpers.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Union
+
 def memoize(f):
     memo = {}
     def helper(index, *args):
@@ -11,3 +13,33 @@ class LLNode:
     def __init__(self, val):
         self.val  = val
         self.next = None
+
+
+def squareGridFromChars(input: List[str], isInts=False) -> Dict[complex, Union[int, str]]:
+    grid = {}
+
+    for j, line in enumerate(input):
+        for i, char in enumerate(line):
+            grid[complex(i, j)] = int(char) if isInts else char
+
+    return grid
+
+def getOrthagonalSquareNeighbors() -> List[complex]:
+    return [
+        0-1j,  # up
+        0+1j,  # down
+        -1+0j, # left
+        1+0j   # right
+    ]
+
+def getAllSquareNeighbors() -> List[complex]:
+    return [
+        -1 - 1j, # up-left
+         0 - 1j, # up
+         1 - 1j, # up-right
+        -1 + 0j, # left
+         1 + 0j, # right
+        -1 + 1j, # down-left
+         0 + 1j, # down
+         1 + 1j, # down-right
+    ]
