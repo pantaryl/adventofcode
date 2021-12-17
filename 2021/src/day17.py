1fe8fd6e665e57a1787571d39291ea7ee06d423e
Michael Grossfeld <pantaryl@gmail.com>
2021-12-17 01:01:53 -0500

    Day17 Solution

---
+++ 2021/src/day17.py
@@ -0,0 +1,37 @@
+from shared import *
+
+# Input data is in INPUT_DATA.
+#INPUT_DATA = [int(x) for x in INPUT_DATA]
+xStart, xEnd, yStart, yEnd = tuple(parse.parse("target area: x={:d}..{:d}, y={:d}..{:d}", INPUT_DATA[0]).fixed)
+
+found = []
+goals = set()
+
+for x in range(xStart, xEnd + 1):
+    for y in range(yStart, yEnd + 1):
+        goals.add(complex(x, y))
+
+for x in range(0, xEnd + 1):
+    for y in range(yStart - 1, abs(yStart) + 1):
+        currentPos = complex(0, 0)
+        startingVelocity = complex(x, y)
+        velocity   = startingVelocity
+        ys = []
+        while currentPos.real <= xEnd and currentPos.imag >= yStart:
+            currentPos += velocity
+            if velocity.real > 0: velocity += complex(-1, 0)
+            elif velocity.real < 0: velocity += complex(1, 0)
+
+            velocity += complex(0, -1)
+
+            ys.append(currentPos.imag)
+            if currentPos in goals:
+                found.append((startingVelocity, int(max(ys))))
+                break
+
+
+# Part 1
+print(max(x[1] for x in found))
+
+# Part 2
+print(len(set([x[0] for x in found])))
