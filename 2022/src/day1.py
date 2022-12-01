601b7225f1a7756834f4b4b55b08d0e77b59ed03
Michael Grossfeld <pantaryl@gmail.com>
2022-12-01 00:04:36 -0500

    Day1 solution

--- 2022/src/day1.py
+++ 2022/src/day1.py
@@ -1,8 +1,22 @@
 from shared import *
 
 # Input data is in INPUT_DATA.
-INPUT_DATA = [int(x) for x in INPUT_DATA]
+INPUT_DATA = [int(x) if x else -1 for x in INPUT_DATA]
 
 # Part 1
+elves = []
+currentSum = 0
+for data in INPUT_DATA:
+    if data == -1:
+        elves.append(currentSum)
+        currentSum = 0
+        continue
+    currentSum += data
+
+elves.append(currentSum)
+
+print(max(elves))
 
 # Part 2
+elves.sort(reverse=True)
+print(sum(elves[:3]))
