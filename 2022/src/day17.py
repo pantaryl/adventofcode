5fde36c01520c53d69ea789c490041f5c06de17f
Michael Grossfeld <pantaryl@gmail.com>
2022-12-17 02:33:57 -0500

    Day17 cleanup

--- 2022/src/day17.py
+++ 2022/src/day17.py
@@ -65,49 +65,30 @@ grid = defaultdict(lambda: '.')
 for x in range(-1, 8):
     grid[x+4j] = '-'
 
-for y in range(-10000000, 5):
+for y in range(-10000, 5):
     grid[complex(-1, y)] = '|'
     grid[complex(7, y)] = '|'
 
-print("Made grid.")
 left_start   = 2+0j
 bottom       = 0+0j
-current_char = None
-current_pos  = []
+current_shape = 0
+current_char  = 0
+current_pos  = None
 # Part 1
 changes = ""
 i = 0
+seen = {}
 def do_fall(upper_bound, part2 = False):
-    global i, changes, current_char, current_pos, bottom
+    global i, changes, current_shape, current_char, current_pos, bottom
     while i < upper_bound:
-        if part2 and i > (len(INPUT_DATA) // 3):
-            substr = longestRepeatedSubstring(changes)
-            num_rocks = len(substr)
-            index  = changes.index(substr)
-            if changes[index + num_rocks:index + 2 * num_rocks] == substr:
-                total_increase = sum([int(x) for x in substr])
-                new_bottom = abs(int(bottom.imag))
-                while i + num_rocks < upper_bound:
-                    new_bottom += total_increase
-                    i += num_rocks
-
-                # Remaining
-                if i < upper_bound:
-                    new_bottom += sum([int(x) for x in substr[0:(upper_bound - i)]])
-                print()
-                print(new_bottom)
-                exit()
-
-        if current_char is None:
-            current_char = shape_list[0]
-            shape_list.rotate(-1)
-
+        if current_pos is None:
             # Need to spawn the shape now
             start = left_start + bottom
-            current_pos = [start + x for x in shapes[current_char]['layout']]
+            shape = shape_list[current_shape % len(shape_list)]
+            current_pos = [start + x for x in shapes[shape]['layout']]
 
-        char = INPUT_DATA[0]
-        INPUT_DATA.rotate(-1)
+        char = INPUT_DATA[current_char % len(INPUT_DATA)]
+        current_char += 1
         dir = directions[char]
         if any([grid[x+dir] != '.' for x in current_pos]):
             # We hit a wall or rocks. This movement doesn't happen.
@@ -121,62 +102,38 @@ def do_fall(upper_bound, part2 = False):
             i += 1
             prev_bottom = bottom
             bottom = complex(0, min([int(x.imag) - 4 for x in current_pos] + [int(bottom.imag)]))
-            changes += f"{int(prev_bottom.imag - bottom.imag)}"
-            #print(f"Found rock {i}, {bottom}, {int(prev_bottom.imag - bottom.imag)}, {current_char}")
+            changes += str(int(prev_bottom.imag - bottom.imag))
+
             # We're going to hit something, so we stop here without moving.
             for x in current_pos:
                 grid[x] = '#'
-            current_char = None
+            current_shape += 1
             current_pos  = None
-            print(f"{i}", end="\r")
+
+            value = (current_shape % len(shape_list), current_char % len(INPUT_DATA), changes[-30:])
+            if part2 and value in seen:
+                rock_idx, new_changes = seen[value]
+
+                new_bottom = abs(int(bottom.imag))
+                cycle_count = i - rock_idx
+                cycle = changes[-cycle_count:]
+                increase_per_cycle = sum([int(x) for x in cycle])
+                remaining_cycles = ((upper_bound - i) // cycle_count)
+                new_bottom += remaining_cycles * increase_per_cycle
+                i += remaining_cycles * cycle_count
+
+                if i < upper_bound:
+                    new_bottom += sum([int(x) for x in cycle[0:(upper_bound - i)]])
+                    i = upper_bound
+                bottom = complex(0, new_bottom)
+            else:
+                seen[value] = (i, changes)
         else:
             current_pos = [x + dir for x in current_pos]
 
 do_fall(2022)
-print(bottom)
 print(abs(int(bottom.imag)))
 
 # Part 2
-# Returns the longest repeating non-overlapping
-# substring in str
-def longestRepeatedSubstring(str):
-    n = len(str)
-    LCSRe = [[0 for x in range(n + 1)]
-             for y in range(n + 1)]
-
-    res = ""  # To store result
-    res_length = 0  # To store length of result
-
-    # building table in bottom-up manner
-    index = 0
-    for i in range(1, n + 1):
-        for j in range(i + 1, n + 1):
-
-            # (j-i) > LCSRe[i-1][j-1] to remove
-            # overlapping
-            if (str[i - 1] == str[j - 1] and
-                    LCSRe[i - 1][j - 1] < (j - i)):
-                LCSRe[i][j] = LCSRe[i - 1][j - 1] + 1
-
-                # updating maximum length of the
-                # substring and updating the finishing
-                # index of the suffix
-                if (LCSRe[i][j] > res_length):
-                    res_length = LCSRe[i][j]
-                    index = max(i, index)
-
-            else:
-                LCSRe[i][j] = 0
-
-    # If we have non-empty result, then insert
-    # all characters from first character to
-    # last character of string
-    if (res_length > 0):
-        for i in range(index - res_length + 1,
-                       index + 1):
-            res = res + str[i - 1]
-
-    return res
-
 do_fall(1000000000000, True)
 print(abs(int(bottom.imag)))
