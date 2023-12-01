d4dd7e179f43c91c735e6d44afe2375d4b405da4
pantaryl <pantaryl@gmail.com>
2023-12-01 00:18:49 -0500

    Solution for Day1.

--- 2023/src/day1.py
+++ 2023/src/day1.py
@@ -1,10 +1,32 @@
-import os
-import __main__
+from shared import *
 
-_MAIN_FILE        =__main__.__file__
-_MAIN_DIR         = os.path.dirname(_MAIN_FILE)
-_MAIN_FILE_NO_EXT = os.path.splitext(os.path.basename(_MAIN_FILE))[0]
+# Input data is in INPUT_DATA.
+NEW_INPUT_DATA = [[int(y) for y in x if y.isdigit()] for x in INPUT_DATA]
 
-INPUT_DATA = []
-with open(f"{_MAIN_DIR}/../input/{_MAIN_FILE_NO_EXT}.txt", 'r') as inputFile:
-    INPUT_DATA = [x.rstrip() for x in inputFile.readlines()]
+print(sum([x[0] * 10 + x[-1] for x in NEW_INPUT_DATA]))
+
+translation = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+part2 = 0
+for i, line in enumerate(INPUT_DATA):
+    numbers = []
+    for j, x in enumerate(line):
+        if x.isdigit():
+            numbers.append(int(x))
+        else:
+            for k, y in enumerate(translation.keys()):
+                if line[j:].startswith(y):
+                    numbers.append(k + 1)
+    part2 += numbers[0] * 10 + numbers[-1]
+
+print(part2)
